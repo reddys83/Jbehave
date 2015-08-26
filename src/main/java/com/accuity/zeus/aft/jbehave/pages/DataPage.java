496343d4625b3fb7745ed4add4d89247913ee4c0
unknown <sahug@SKOW101002.b2b.regn.net>
2015-08-26 11:07:30 -0500

    ZEUS-251-country-credit-ratings-scenarios

--- src/main/java/com/accuity/zeus/aft/jbehave/pages/DataPage.java
+++ src/main/java/com/accuity/zeus/aft/jbehave/pages/DataPage.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -117,9 +116,17 @@ public class DataPage extends AbstractPage {
     private By country_regions_value_label_xpath = By.xpath("//li[contains(h2,'Alternative Regions')]//tr/th[2]");
     private By country_regions_type_xpath = By.xpath("//li[contains(h2,'Alternative Regions')]//tr/td[1]");
     private By country_regions_value_xpath = By.xpath("//li[contains(h2,'Alternative Regions')]//tr/td[2]");
-
-
+    private By country_credit_rating_id = By.id("creditRating");
     private String selectedCountry = "";
+    private By country_credit_rating_label_xpath = By.xpath("//li[contains(h1,'Credit Rating')]/h1/span");
+    private By country_credit_rating_for_label_xpath = By.xpath("//li[contains(h1,'Credit Rating')]/h2");
+    private By country_credit_rating_table_headers_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//thead/tr");
+    private By country_credit_rating_agency_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//tbody/tr/td[1]");
+    private By country_credit_rating_type_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//tbody/tr/td[2]");
+    private By country_credit_rating_value_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//tbody/tr/td[3]");
+    private By country_credit_rating_applied_date_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//tbody/tr/td[4]");
+    private By country_credit_rating_confirmed_date_xpath = By.xpath("//li[contains(h1,'Credit Rating')]//tbody/tr/td[5]");
+
     public DataPage(WebDriver driver, String urlPrefix) {
         super(driver, urlPrefix);
     }
@@ -540,5 +547,36 @@ public class DataPage extends AbstractPage {
             assertEquals(countryRegions.getRow(i).get(countryRegions.getHeaders().get(0)),regionType.get(i).getText());
             assertEquals(countryRegions.getRow(i).get(countryRegions.getHeaders().get(1)),regionValue.get(i).getText());
         }
+    }
+
+    public void clickOnCountryCreditRating() {
+        attemptClick(country_credit_rating_id);
+    }
+
+    public void countryCreditRatings(ExamplesTable countryCreditRatings) {
+        assertEquals("CREDIT RATING", getDriver().findElement(country_credit_rating_label_xpath).getText());
+        assertEquals("CREDIT RATINGS FOR " + selectedCountry.toUpperCase(), getDriver().findElement(country_credit_rating_for_label_xpath).getText());
+        assertEquals("AGENCY TYPE VALUE APPLIED DATE CONFIRMED DATE", getDriver().findElement(country_credit_rating_table_headers_xpath).getText());
+        List<WebElement> agency = getDriver().findElements(country_credit_rating_agency_xpath);
+        List<WebElement> type = getDriver().findElements(country_credit_rating_type_xpath);
+        List<WebElement> value = getDriver().findElements(country_credit_rating_value_xpath);
+        List<WebElement> appliedDate = getDriver().findElements(country_credit_rating_applied_date_xpath);
+        List<WebElement> confirmedDate = getDriver().findElements(country_credit_rating_confirmed_date_xpath);
+        for (int i=0; i<countryCreditRatings.getRowCount(); i++){
+            assertEquals(countryCreditRatings.getRow(i).get(countryCreditRatings.getHeaders().get(0)),agency.get(i).getText());
+            assertEquals(countryCreditRatings.getRow(i).get(countryCreditRatings.getHeaders().get(1)),type.get(i).getText());
+            assertEquals(countryCreditRatings.getRow(i).get(countryCreditRatings.getHeaders().get(2)),value.get(i).getText());
+            assertEquals(countryCreditRatings.getRow(i).get(countryCreditRatings.getHeaders().get(3)),appliedDate.get(i).getText());
+            assertEquals(countryCreditRatings.getRow(i).get(countryCreditRatings.getHeaders().get(4)),confirmedDate.get(i).getText());
+        }
+    }
+
+    public void verifyNoCountryCreditRatings() {
+        assertEquals("CREDIT RATING", getDriver().findElement(country_credit_rating_label_xpath).getText());
+        assertEquals("CREDIT RATINGS FOR " + selectedCountry.toUpperCase(), getDriver().findElement(country_credit_rating_for_label_xpath).getText());
+        assertEquals("AGENCY TYPE VALUE APPLIED DATE CONFIRMED DATE", getDriver().findElement(country_credit_rating_table_headers_xpath).getText());
+        try {
+            assertFalse(getDriver().findElement(country_credit_rating_agency_xpath).isDisplayed());
+        } catch (org.openqa.selenium.NoSuchElementException e){}
     }
 }
