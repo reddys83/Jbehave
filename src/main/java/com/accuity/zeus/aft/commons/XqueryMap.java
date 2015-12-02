66f4d4a4c8bd6c73b2aed3d4b7fdedea298f1f12
gajendra.sahu@bankersaccuity.com <gajendra.sahu@bankersaccuity.com>
2015-12-02 14:45:39 -0600

    added-missing-mapping

--- src/main/java/com/accuity/zeus/aft/commons/XqueryMap.java
+++ src/main/java/com/accuity/zeus/aft/commons/XqueryMap.java
@@ -16,6 +16,7 @@ public class XqueryMap {
         queryMap.put("descending order by office status","sortDescByOfficeStatus.xqy");
         queryMap.put("ascending order by office status", "sortAscByOfficeStatus.xqy");
         queryMap.put("country list","getCountryList.xqy");
+        queryMap.put("multiple office types sorted alphabetically", "multipleOfficeTypesAlphabetically.xqy");
         queryMap.put("ascending order by office type","sortAscByOfficeTypes.xqy");
         queryMap.put("descending order by office type","sortDescByOfficeTypes.xqy");
         queryMap.put("domestic offices list", "getDomesticOfficesList.xqy");
