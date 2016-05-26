a88649031fd3f67f3db7917899a56983524fbe39
satyaaditya <satyaaditya.nm@cognizant.com>
2016-05-26 20:44:00 +0530

    updating city steps

--- src/main/java/com/accuity/zeus/aft/jbehave/steps/EditCitySteps.java
+++ src/main/java/com/accuity/zeus/aft/jbehave/steps/EditCitySteps.java
@@ -1,45 +1,76 @@
 package com.accuity.zeus.aft.jbehave.steps;
 
-import org.jbehave.core.annotations.When;
-import org.jbehave.core.annotations.Named;
-import org.jbehave.core.annotations.Then;
+import com.accuity.zeus.aft.io.ApacheHttpClient;
+import com.accuity.zeus.aft.io.Database;
+import org.eclipse.jetty.util.annotation.Name;
+import org.jbehave.core.annotations.*;
+import org.jbehave.core.model.ExamplesTable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class EditCitySteps extends AbstractSteps {
 
-/**
- * Created by shahc1 on 5/19/2016.
- */
-public class EditCitySteps extends AbstractSteps{
+	@Autowired
+	ApacheHttpClient apacheHttpClient;
+	@Autowired
+	Database database;
 
-    @When("the user clicks on the city update link")
-    public void clickOnUpdateCurrencyLink() {
-        getDataPage().clickOnUpdateCurrencyLink();
-        if(editCityPage==null){
-            editCityPage = getDataPage().createEditCityPage();
-            
-        }}
-    
-    @When("the user clicks on the add new identifier button in the basic info city page")
-    public void clickOnAddNewIdentifierButton(){
-        getCountryPage().clickOnAddNewIdentifierButton();
-    }
-    
-    @Then("the user should see the default identifier status as active")
-    public void assertIdentifierStatus(){
-        getCountryPage().verifyIdentifierStatus();
-    }
-    
-    @When("the user enters identifier type as <identifierType> in the basic info city page")
-    public void enterIdentifierType(@Named("identifierType") String identifierType) {
-        getCountryPage().enterIdentifierType(identifierType);
-    }
+	@When("the user clicks on the add new identifier button in the basic info city page")
+	public void clickOnAddNewIdentifirButton() {
+		getEditCityPage().clickOnAddNewIdentifirButton();
+	}
 
-    @When("the user enters identifier value as <identifierValueIncorrect> in the basic info city page")
-    public void enterIdentifierValue(@Named("identifierValueIncorrect") String incorrectIdentifierValue) {
-        getCountryPage().enterIdentifierValue(incorrectIdentifierValue);
-    }
-    
-    @When("the user clears identifier value in the basic info city page")
-    public void clearCountryIdentifierValue(){
-        getCountryPage().clearCountryIdentifierValue();
-    }
+	 @When("the user enters identifier type as <identifierType> in the basic info city page")
+	    public void enterIdentifierType(@Named("identifierType") String identifierType) {
+	    	getEditCityPage().enterIdentifierType(identifierType);
+	    }
+	 
+	 @When("the user enters identifier status as <identifierStatus> in the basic info city page")
+	    public void enterIdentifierStatus(@Named("identifierStatus") String identifierStatus) {
+		 getEditCityPage().enterIdentifierStatus(identifierStatus);
+	        
+	    }
+	 
+	 @When("the user clicks on the delete identifier row button in the basic info city page")
+	    public void clickOnDeleteNewIdentifierRowButtonCity(){
+		 getEditCityPage().clickOnDeleteNewIdentifierRowButtonCity();
+	    }
+	 
+	 @Then("the user should not see the newly added identifier row in the basic info city page")
+	    public void verifyNewlyAdedIdentifierRowIsNotDisplayed() {
+		 getEditCityPage().verifyNewlyAdedIdentifierRowIsNotDisplayed();
+	    }
+	 
+	   @Then("the user should see delete row confirmation modal in the city page")
+	    public void verifyDeleteConfirmationModal(){
+		 getEditCityPage().verifyDeleteConfirmationModal();
+	    }
+	 
+	 @When("the user enters identifier value as <identifierValueIncorrect> in the basic info city page")
+	    public void enterIdentifierValue(@Named("identifierValueIncorrect") String incorrectIdentifierValue) {
+		 getEditCityPage().enterIdentifierValue(incorrectIdentifierValue);
+	    }
+	 
+	 @When("the user clears identifier value in the basic info city page")
+	    public void clearCityIdentifierValue(){
+		 getEditCityPage().clearCityIdentifierValue();
+	    }
+	 
+	 @When("the user enters identifier value as <identifierValue> in the basic info city page")
+	    public void enterIdentifierValues(@Named("identifierValue") String identifierValue) {
+		 getEditCityPage().enterIdentifierValue(identifierValue);
+	    }
+	 
+	 @When("the user clicks on the confirm button in city page")
+	    public void clickOnConfirmButtonCity() throws Exception {
+		 getEditCityPage().clickOnConfirmButtonCity();
+	    }
+	 
+	  	 
+	 @Then("the user should see the updated identifiers in the City page as $identifierType $identifierValue $identifierStatus")
+	    public void verifyUpdateSuccessMessage(@Named("identifierType") String identifierType,@Named("identifierValue") String identifierValue,@Named("identifierStatus") String identifierStatus) throws Exception {
+		  getEditCityPage().verifyUpdateSuccessIdentifiers(identifierType,identifierValue,identifierStatus);
+	    }
+	 
 }
