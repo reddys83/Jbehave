cb2f711b70307e5710602bdc757bb02bdb939ef5
Saptarshi Sukhendu Das <Saptarshi.Das3@cognizant.com>
2016-05-26 10:53:51 +0530

    Added xpath in CityIdentifiers.

--- src/main/java/com/accuity/zeus/aft/jbehave/identifiers/CityIdentifiers.java
+++ src/main/java/com/accuity/zeus/aft/jbehave/identifiers/CityIdentifiers.java
@@ -12,9 +12,9 @@ public class CityIdentifiers {
     private static HashMap<String, By> hmap = new HashMap<String, By>();
 
     public CityIdentifiers(){
-       hmap.put("city_add_info_text_xpath",By.xpath(("")));
-       hmap.put("city_save_confirmation_message_id",By.id(("")));
-       hmap.put("city_addInfo_error_message_edit_xpath", By.xpath(""));
+       hmap.put("city_add_info_text_xpath",By.xpath(("//*[@id='cityBasicInfo']/ul/li[2]/table/tbody/tr[10]/td/textarea")));
+       hmap.put("city_save_confirmation_message_id",By.id(("//*[@id='saveSuccess']")));
+       hmap.put("city_addInfo_error_message_edit_xpath", By.xpath("//*[@id='data']/div/ul/li[1]/ul/li[2]/table/tbody/tr[10]/td/p"));
     }
     public static By getObjectIdentifier(String key) {
 
