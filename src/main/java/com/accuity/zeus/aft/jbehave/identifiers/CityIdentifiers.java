c8efc7379d53f9e153e178a2b70f435503b80e7f
satyaaditya <satyaaditya.nm@cognizant.com>
2016-06-01 17:48:07 +0530

    updating the identifiers with Xpath

--- src/main/java/com/accuity/zeus/aft/jbehave/identifiers/CityIdentifiers.java
+++ src/main/java/com/accuity/zeus/aft/jbehave/identifiers/CityIdentifiers.java
@@ -9,10 +9,10 @@ public class CityIdentifiers {
 	private static HashMap<String, By> hmap = new HashMap<String, By>();
 
 	public static void setIdentifiers() {
-		hmap.put("city_population_input_id", By.cssSelector(("input[name='population']")));		
-		hmap.put("city_save_confirmation_message_id", By.cssSelector(("#saveSuccess")));				
-		hmap.put("save_button_id", By.cssSelector("#save-button"));
-		hmap.put("city_population_error_message_id", By.cssSelector("p[data-error_id='populationError']"));		
+		hmap.put("city_population_input_id", By.xpath("//*[@id='cityBasicInfo']/ul/li[2]/table/tbody/tr[8]/td/input"));	
+		hmap.put("city_save_confirmation_message_id", By.xpath("//p[@id='saveSuccess']"));				
+		hmap.put("save_button_id",By.xpath("//button[@id='save-button']"));
+		hmap.put("city_population_error_message_id", By.xpath("//li[@id='cityBasicInfo']/ul/li[2]/table/tbody/tr[8]/td/p"));		
 	}
 
 	public static By getObjectIdentifier(String key) {
