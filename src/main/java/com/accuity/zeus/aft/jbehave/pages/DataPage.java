95b53ba4a041873b64c747f078cfa2e6704fe5a8
satyaaditya <satyaaditya.nm@cognizant.com>
2016-08-25 19:22:22 +0530

    ZEUS-1047 Committing the code after final execution

--- src/main/java/com/accuity/zeus/aft/jbehave/pages/DataPage.java
+++ src/main/java/com/accuity/zeus/aft/jbehave/pages/DataPage.java
@@ -4,7 +4,6 @@ import com.accuity.zeus.aft.commons.ParamMap;
 import com.accuity.zeus.aft.io.ApacheHttpClient;
 import com.accuity.zeus.aft.io.Database;
 import com.accuity.zeus.aft.io.HeraApi;
-import com.accuity.zeus.aft.jbehave.identifiers.AreaIdentifiers;
 import com.accuity.zeus.aft.jbehave.identifiers.LegalEntityIdentifiers;
 import com.accuity.zeus.aft.jbehave.identifiers.OfficeIdentifiers;
 import com.accuity.zeus.aft.rest.Response;
@@ -1373,16 +1372,16 @@ public class DataPage extends AbstractPage {
 		}
 	}
 	
-	public void verifyLookUpValues(By by,String xqueryName,String tagName) {
-		List<WebElement> regionTypeList = getDriver().findElements(by);
+	public void verifyLookUpValues(By by, String xqueryName, String tagName) {
+		List<WebElement> elementTypeList = getDriver().findElements(by);
 		Document document = apacheHttpClient.executeDatabaseAdminQueryWithResponse(database, xqueryName);
 		assertTrue(document.getElementsByTagName(tagName).getLength() > 1);
 		for (int i = 1; i < document.getElementsByTagName(tagName).getLength(); i++) {
 			assertEquals(document.getFirstChild().getChildNodes().item(i).getFirstChild().getTextContent(),
-					regionTypeList.get(i).getAttribute("value"));
+					elementTypeList.get(i).getAttribute("value"));
 		}
 	}
-	
+
 	public void clickOnWebElement(By by) {
 		  attemptClick(by);
 		 }
